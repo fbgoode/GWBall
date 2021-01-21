63f6f5bd9e688ed40de1496f5b3c134c700a7191
Fede <fedebaez@gmail.com>
2021-01-21 21:01:34 +0100

    Intento de arreglar sonido en GitHub Pages.

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 document.addEventListener('keydown', inputManager);
 
-var beepM = new Audio("../wav/move.wav");
-var beepC = new Audio("../wav/catch.wav");
-var beepF = new Audio("../wav/fail.wav");
+var beepM = new Audio("/wav/move.wav");
+var beepC = new Audio("/wav/catch.wav");
+var beepF = new Audio("/wav/fail.wav");
 const screenObj = document.getElementById("screen");
 const bodyObj = document.getElementById("body");
 const scoreObj = document.getElementById("score");
